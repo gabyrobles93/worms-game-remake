69025ae6f515290f022169e028e14fdce142c3f5
Gonza <dizmartin.gonzalo@gmail.com>
2018-05-10 18:30:06 -0300

    actualizo cmake

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
+sudo apt-get install libsdl2-ttf-dev
+sudo apt-get install libsdl2-mixer-dev
 sudo apt-get install cmake
 mkdir build && cd build
 cmake ..
