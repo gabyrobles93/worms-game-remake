b88b190b9145c352dac10e79c38c4fc95194b17d
arieljaw12 <arieljaw12@gmail.com>
2018-05-12 14:26:03 -0300

    Agregue la clase View::Girder, ShortGirder, Exceptions, Texture... Probe dibujar las vigas e ir rotandolas y funciona.

---
+++ test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/girder.cpp" "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/girder.cpp.o"
+  "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/girder_short.cpp" "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/girder_short.cpp.o"
+  "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/main.cpp" "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/main.cpp.o"
+  "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/texture.cpp" "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/texture.cpp.o"
+  "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/view_exceptions.cpp" "/home/ariel/FIUBA/TallerDeProgramacion/tps/tpfinal/tp-final-taller/test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/view_exceptions.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test_cases/test_sdl/build/CMakeFiles/test_sdl.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_sdl.dir/main.cpp.o"
+  "CMakeFiles/test_sdl.dir/girder.cpp.o"
+  "CMakeFiles/test_sdl.dir/girder_short.cpp.o"
+  "CMakeFiles/test_sdl.dir/texture.cpp.o"
+  "CMakeFiles/test_sdl.dir/view_exceptions.cpp.o"
+  "test_sdl.pdb"
+  "test_sdl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_sdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
