6605f23db55f1b982d980b933e2a65edb63dce42
gabyrobles93 <gabyrobles93@gmail.com>
2018-05-14 13:40:37 -0300

    Fix en el cmake del cliente.

---
+++ client/build/CMakeFiles/client_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "client_autogen"
+  "CMakeFiles/client_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/client_autogen"
+  "ui_client_connector.h"
+  "ui_client_lobby.h"
+  "qrc_client_resources.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/client_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
