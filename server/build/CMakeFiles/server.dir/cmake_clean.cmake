84086d23b96ed718b26c4610d6875b3ffa93aea6
Gabriel Robles <gabyrobles93@gmail.com>
2018-05-20 14:07:01 -0300

    Comento las dependencias de box2d del cmake del servidor, total lo que necesito testear ahora no usa box2d.

---
+++ server/build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/home/grobles/tp-final-taller/common/src/socket.cpp.o"
+  "CMakeFiles/server.dir/home/grobles/tp-final-taller/common/src/socket_error.cpp.o"
+  "CMakeFiles/server.dir/home/grobles/tp-final-taller/common/src/thread.cpp.o"
+  "CMakeFiles/server.dir/home/grobles/tp-final-taller/common/src/protocol.cpp.o"
+  "CMakeFiles/server.dir/home/grobles/tp-final-taller/common/src/protocol_error.cpp.o"
+  "server.pdb"
+  "server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
