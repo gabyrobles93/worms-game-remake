f767cdf96670c7043e90e887e6361598d1a238b8
gabyrobles93 <gabyrobles93@gmail.com>
2018-05-23 18:07:15 -0300

    Agrego folder para desarrollar el editor.

---
+++ server/src/team.h
@@ -0,0 +1,16 @@
+#ifndef __TEAM_H__
+#define __TEAM_H__
+
+#include <map>
+#include "Worm.h"
+
+class Team {
+    private:
+        size_t team_id;
+        std::map<size_t, Worm*> & worms;
+
+    public:
+        Team(size_t, )
+};
+
+#endif
