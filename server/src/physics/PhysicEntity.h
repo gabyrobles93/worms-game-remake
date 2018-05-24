0024eb70686b4d67b4c1e6b05d8018c4e322e8c9
gonza <dizmartin.gonzalo@gmail.com>
2018-05-24 20:18:15 -0300

    crea PhysicEntity, todos los cuerpos fisicos deberian heredar de physicentity

---
+++ server/src/physics/PhysicEntity.h
@@ -0,0 +1,17 @@
+#ifndef PHYSIC_ENTITY_H
+#define PHYSIC_ENTITY_H
+
+#include "Box2D.h"
+
+enum _entityCategory {
+    WORM = 0x0001,
+    STRUCTURE = 0x0002,
+    WATER = 0x0004,
+};
+
+class PhysicEntity {
+public:
+private:
+};
+
+#endif
