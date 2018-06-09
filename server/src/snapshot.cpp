21736690d024c63754197502d52d684b20a5c2bf
Gonza <dizmartin.gonzalo@gmail.com>
2018-06-09 11:46:23 -0300

    bugfix :wave: de blast radius

--- server/src/snapshot.cpp
+++ server/src/snapshot.cpp
@@ -111,7 +111,7 @@ void Snapshot::updateProjectiles(std::map<int, Weapon*> & weapons) {
         snapshot << YAML::Key << "y" << YAML::Value << (int) (weapon->getPosY() /  gConfiguration.SCALING_FACTOR);
         snapshot << YAML::Key << "countdown" << YAML::Value << weapon->getCountdown();
         snapshot << YAML::Key << "exploded" << YAML::Value << (int) weapon->hasExploded();
-        snapshot << YAML::Key << "blast_radius" << YAML::Value << weapon->getBlastRadius();
+        snapshot << YAML::Key << "blast_radius" << YAML::Value << (int) (weapon->getBlastRadius() / gConfiguration.SCALING_FACTOR);
         snapshot << YAML::EndMap;
     }
     snapshot << YAML::EndSeq;
