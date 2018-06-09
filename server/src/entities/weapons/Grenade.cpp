cb9192a3ee4ba43bc96639e6602a80b63ddd65cb
Gonza <dizmartin.gonzalo@gmail.com>
2018-06-09 12:47:41 -0300

    Refactor granadas

--- server/src/entities/weapons/Grenade.cpp
+++ server/src/entities/weapons/Grenade.cpp
@@ -4,7 +4,24 @@
 Grenade::Grenade(int id, b2World& world, float posX, float posY, bool mirrored, float shooting_angle, int power_factor, int delay, int currentTime, weapon_t type) :
 Weapon(type),
 detonationTime(currentTime + delay),
-grenadePhysic(world, posX, posY) {
+world(world) {
+    b2BodyDef grenadeDef;
+    grenadeDef.type = b2_dynamicBody;
+    grenadeDef.position.Set(posX, posY);
+    b2Body* body = world.CreateBody(&grenadeDef);
+
+    b2CircleShape grenadeShape;
+    grenadeShape.m_radius = GRENADE_RADIUS;
+
+    b2FixtureDef grenadeFixture;
+    grenadeFixture.shape = &grenadeShape;
+    grenadeFixture.density = GRENADE_DENSITY;
+    grenadeFixture.friction = 50000000;
+    grenadeFixture.restitution = gConfiguration.GRENADE_RESTITUTION;
+    body->CreateFixture(&grenadeFixture);
+
+    this->body = body;
+
     this->exploded = false;
     this->power_factor = power_factor;
     this->mirrored = mirrored;
@@ -24,6 +41,10 @@ grenadePhysic(world, posX, posY) {
     shoot();
 }
 
+Grenade::~Grenade() {
+    this->world.DestroyBody(this->body);
+}
+
 void Grenade::update(int currentTime) {
     if (currentTime >= this->detonationTime && !exploded) {
         explode();
@@ -32,18 +53,24 @@ void Grenade::update(int currentTime) {
 }
 
 void Grenade::explode() {
-    this->grenadePhysic.explode(this->blast_radius, this->blast_power);
+    ExplosionManager explosioManager(this->world);
+    b2Vec2 center = this->body->GetPosition();
+    explosioManager.manageExplosion(center, blast_radius, blast_power);
     this->exploded = true;
 }
 
 float Grenade::getPosX() {
-    return this->grenadePhysic.getPosX();
+    return this->body->GetPosition().x;
 }
 
 float Grenade::getPosY() {
-    return this->grenadePhysic.getPosY();
+    return this->body->GetPosition().y;
 }
 
 void Grenade::shoot() {
-    this->grenadePhysic.shoot(mirrored, shooting_angle, power_factor);
+    float impulse = this->body->GetMass() * power_factor;
+    float x_impulse = cosf(shooting_angle * gConfiguration.DEGTORAD) * impulse;
+    float y_impulse = sinf(shooting_angle * gConfiguration.DEGTORAD) * impulse;
+    if (!mirrored) x_impulse = x_impulse * -1;
+    this->body->ApplyLinearImpulse(b2Vec2(x_impulse, -y_impulse), this->body->GetWorldCenter(), true);
 }
--- server/src/entities/weapons/Grenade.h
+++ server/src/entities/weapons/Grenade.h
@@ -3,22 +3,26 @@
 
 #include "Box2D.h"
 #include "Weapon.h"
-#include "GrenadePhysic.h"
 #include "types.h"
 #include "Configuration.h"
+#include "ExplosionManager.h"
+
+#define GRENADE_RADIUS 0.25f
+#define GRENADE_DENSITY 1
 
 class Grenade : public Weapon {
 private:    
     int detonationTime;
-    weapon_t type;
+    b2World & world;
+    b2Body* body;
     int blast_power;
     bool mirrored;
     int shooting_angle;
     int power_factor;
-    GrenadePhysic grenadePhysic;
     void explode(void);
 public:
     Grenade(int id, b2World& world, float posX, float posY, bool mirrored, float shooting_angle, int power_factor, int delay, int currentTime, weapon_t type);
+    ~Grenade();
     void shoot(void);
     void update(int currentTime);
     float getPosX();
