41cc2589cbedc598c06d79e94741f27d7f2d6ece
Gonza <dizmartin.gonzalo@gmail.com>
2018-06-09 12:40:02 -0300

    Modifica dynamite

--- server/src/entities/weapons/Dynamite.h
+++ server/src/entities/weapons/Dynamite.h
@@ -1,18 +1,25 @@
 #ifndef DYNAMITE_H
 #define DYNAMITE_H
 
-#include "DynamitePhysic.h"
+
 #include "Weapon.h"
 #include "Box2D/Box2D.h"
 #include "types.h"
 #include "Configuration.h"
+#include "ExplosionManager.h"
+
+#define DYNAMITE_WIDTH 0.2f
+#define DYNAMITE_HEIGHT 0.4f
 
 class Dynamite : public Weapon{
 private: 
     int detonationTime;
-    DynamitePhysic dynamitePhysic;
+    b2World& world;
+    b2Body* body;
+    int blast_power;
 public:
     Dynamite(int id, b2World& world, float posX, float posY, int delay, int currentTime);
+    ~Dynamite();
     void explode(void);
     void update(int currentTime);
     bool hasExploded(void);
