09bb1dc0616ce68f1a700a68ede6dfec228e34f8
Gonza <dizmartin.gonzalo@gmail.com>
2018-06-14 13:31:27 -0300

    modificando explosion manager

--- server/src/physics/ExplosionManager.cpp
+++ server/src/physics/ExplosionManager.cpp
@@ -20,7 +20,7 @@ void ExplosionManager::manageExplosion(b2Vec2 center, float radius, float power)
     for (unsigned int i = 0; i < queryCallback.foundBodies.size(); i++) {
         b2Body* body = queryCallback.foundBodies[i];
         b2Vec2 bodyCom = body->GetWorldCenter();
-        if ((bodyCom - center).Length() >= radius)
+        if ((bodyCom - center).Length() > radius)
             continue;
         this->applyBlastImpulse(body, center, bodyCom, power);
     }
@@ -29,10 +29,15 @@ void ExplosionManager::manageExplosion(b2Vec2 center, float radius, float power)
 void ExplosionManager::applyBlastImpulse(b2Body* body, b2Vec2 blastCenter, b2Vec2 applyPoint, float blastPower) {
 	b2Vec2 blastDir = applyPoint - blastCenter;
 	float distance = blastDir.Normalize();
+    if (blastDir.y > 0) 
+        blastDir.y = -blastDir.y;
 	if (distance == 0)
 		return;
 	float invDistance = 1/distance;
-	float impulseMag = blastPower * invDistance * invDistance / REDUCE_FACTOR;
+	float impulseMag = (blastPower/5) * invDistance; // * invDistance; // * invDistance;// / REDUCE_FACTOR;
+    
+    std::cout << "DISTANCE " << distance << "iMpulseMAge" << impulseMag << std::endl;
+    std::cout << "BLASTDIR.y " << blastDir.y << "BLASTDIR.x" << blastDir.x << std::endl;
     entity_t entity_type = static_cast<Entity*>(body->GetUserData())->getEntityType();
     if (entity_type == WORM) {
         body->ApplyLinearImpulse(impulseMag * blastDir, applyPoint, true);
